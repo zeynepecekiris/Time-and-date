e15a62c6ad6ad726167c4665dc24a62fc6cacc13
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-27 18:16:06 +0300

    first commit

---
+++ time.js
@@ -0,0 +1,16 @@
+const now=new Date();
+console.log(now);
+
+console.log(typeof now);
+
+console.log("year:",now.getFullYear());
+console.log("month:",now.getMonth());
+console.log("date:",now.getDate());
+console.log("day:", now.getDay());
+console.log("Hours", now.getHours());
+console.log("minutes", now.getMinutes());
+
+
+console.log("times")
+
+
